6eb300fd2ba5adfe4d61c331f958a7ca18a0623b
moon <237253995@qq.com>
2015-11-09 15:19:34 +0800

    +1

--- app/src/main/java/com/moon/myreadapp/ui/LoginActivity.java
+++ app/src/main/java/com/moon/myreadapp/ui/LoginActivity.java
@@ -3,21 +3,18 @@ package com.moon.myreadapp.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -30,14 +27,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.moon.appframework.common.business.RequestHelper;
 import com.moon.myreadapp.R;
 import com.moon.myreadapp.ui.base.BaseActivity;
 
-import org.antlr.v4.Tool;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
